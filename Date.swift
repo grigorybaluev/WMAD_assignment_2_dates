b20dbd5fb8b81e0e3e85dfcf718b0dd83ddd1d14
grigorybaluev <gr.baluev@gmail.com>
2023-03-09 10:18:55 -0500

    add files

---
+++ Date.swift
@@ -0,0 +1,17 @@
+//
+//  Date.swift
+//  Assignment2
+//
+//  Created by Derrick Park on 2023-03-03.
+//
+
+import Foundation
+
+struct Date {
+  
+}
+
+enum DateFormat {
+  case standard, long, two
+}
+
